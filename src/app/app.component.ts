dd49e8862d640f3e923ba64cc3e45101d45cf059
Salima-POEC-JAVA <salimamabrouk7@gmail.com>
2024-11-27 17:38:49 +0100

    wip nav et footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./components/nav/nav.component";
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
