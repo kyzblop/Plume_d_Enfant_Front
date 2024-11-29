bee9c443135a87e9f7348ade916606a033a71d31
christophe-miranville <christophe.miranville@laposte.net>
2024-11-29 14:04:25 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { AdminPageComponent } from "./components/admin-page/admin-page.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { CreatePageComponent } from "./components/create-page/create-page.component";
+import { StoriesPageComponent } from "./components/stories-page/stories-page.component";
+import { ProfilPageComponent } from "./components/profil-page/profil-page.component";
+import { StoryGeneratedPageComponent } from "./components/story-generated-page/story-generated-page.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "create", component: CreatePageComponent },
+  { path: "stories", component: StoriesPageComponent },
+  { path: "profil", component: ProfilPageComponent },
+  { path: "story-generated", component: StoryGeneratedPageComponent },
+  { path: "admin", component: AdminPageComponent },
+  { path: "404", component: NotFoundComponent },
+  { path: "**", redirectTo: "/404" }
+];
--- src/app/components/create-page/create-page.component.ts
+++ src/app/components/create-page/create-page.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
+import { log } from "console";
 
 @Component({
-  selector: 'app-create-page',
+  selector: "app-create-page",
   standalone: true,
-  imports: [],
-  templateUrl: './create-page.component.html',
-  styleUrl: './create-page.component.css'
+  imports: [FormsModule, CommonModule],
+  templateUrl: "./create-page.component.html",
+  styleUrl: "./create-page.component.css"
 })
 export class CreatePageComponent {
+  prenomPrincipal: string = "";
+  prenomSecondaire: string = "";
+  detailPrincipal: string = "";
+  detailSecondaire: string = "";
+  detailSupp1: string = "";
+  detailSupp2: string = "";
+  detailSupp3: string = "";
+  detailSupp4: string = "";
+  category: string = "Aléatoire";
+  age: any;
+  personnagesSecondaires: any;
 
+  prenomSecondaire1: any;
+  prenomSecondaire2: any;
+  relationSecondaire2: any;
+  relationPrincipale1: any;
+
+  onSubmit(storyForm: NgForm) {
+    console.log(storyForm.value.prenomPrincipal);
+    storyForm.reset();
+  }
 }
