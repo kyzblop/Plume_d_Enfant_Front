1fcb79bfddbde5f6c4cccf77cbe08876f06740e6
christophe-miranville <christophe.miranville@laposte.net>
2024-12-02 18:23:16 +0100

    start_card_dyna

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { CardComponent } from '../card/card.component';
+import { Component } from "@angular/core";
+import { CardComponent } from "../card/card.component";
+import { RouterLink } from "@angular/router";
+import { Histoire } from "../../model/histoire";
 
 @Component({
-  selector: 'app-home-page',
+  selector: "app-home-page",
   standalone: true,
-  imports: [CardComponent],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  imports: [CardComponent, RouterLink],
+  templateUrl: "./home-page.component.html",
+  styleUrl: "./home-page.component.css"
 })
 export class HomePageComponent {
-
+  histoires: Histoire[] = [];
 }
