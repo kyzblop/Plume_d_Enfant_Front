cf8b8bdbda91691bd68b7861bebcc13dfc577c64
kyz blop <jeremymoi.jd@gmail.com>
2024-12-14 15:23:46 +0100

    url back maj

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,7 +10,8 @@ import { Utilisateur } from '../model/utilisateur';
   providedIn: 'root',
 })
 export class AuthService {
-  readonly apiUrl = 'http://localhost:8080';
+  readonly apiUrl = 'https://plumedenfant-production.up.railway.app';
+  // readonly apiUrl = 'http://localhost:8080';
 
   public isAuthSubject = new BehaviorSubject<boolean>(this.isAuthenticated());
   public isAuthObservable = this.isAuthSubject.asObservable();
